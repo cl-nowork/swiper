804c07e254f1317d46c1ec1f03b71a7da50304f6
seelow <seelow_cl@163.com>
2021-05-10 16:57:39 +0000

    添加profile 交友资源模型

--- user/models.py
+++ user/models.py
@@ -31,3 +31,20 @@ class User(models.Model):
             'avatar': self.avatar,
             'location': self.location,
         }
+
+
+class Profile(models.Model):
+    '''交友资料'''
+    dating_sex = models.CharField(max_length=8, choices=User.SEX, verbose_name='匹配的性别')
+    dating_location = models.CharField(max_length=20, choices=User.LOCATION, verbose_name='目标城市')
+    min_dating_age = models.IntegerField(default=18, verbose_name='最小交友年龄')
+    max_dating_age = models.IntegerField(default=50, verbose_name='最大交友年龄')
+    min_distance = models.IntegerField(default=1, verbose_name='最小查找范围')
+    max_distance = models.IntegerField(default=30, verbose_name='最大查找范围')
+
+    vibration = models.BooleanField(default=True, verbose_name='是否开启震动')
+    only_matched = models.BooleanField(default=True, verbose_name='是否只让匹配的人看到')
+    auto_play = models.BooleanField(default=True, verbose_name='是否自动播放')
+
+    class Meta:
+        db_table = 'profile'
